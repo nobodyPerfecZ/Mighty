b0123bae7d0872108ddcafab70266f0664352ad8
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2025-04-14 14:00:14 +0200

    Deployed e7f1de0 with MkDocs version: 1.6.1

---
+++ hooks/cleanup_log_output.py
@@ -0,0 +1,34 @@
+"""The module is a hook which disables warnings and log messages which pollute the
+doc build output.
+
+One possible downside is if one of these modules ends up giving an actual
+error, such as OpenML failing to retrieve a dataset. I tried to make sure ERROR
+log message are still allowed through.
+"""
+import logging
+import warnings
+from typing import Any
+
+import mkdocs
+import mkdocs.plugins
+import mkdocs.structure.pages
+
+log = logging.getLogger("mkdocs")
+
+
+@mkdocs.plugins.event_priority(-50)
+def on_startup(**kwargs: Any):
+    # We can probably safely disregard these
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+def on_pre_page(
+    page: mkdocs.structure.pages.Page,
+    config: Any,
+    files: Any,
+) -> mkdocs.structure.pages.Page | None:
+    # NOTE: mkdocs says they're always normalized to be '/' seperated
+    # which means this should work on windows as well.
+
+    logging.getLogger("mighty").setLevel(logging.ERROR)
+    return page
