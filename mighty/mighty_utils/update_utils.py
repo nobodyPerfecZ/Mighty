41658e834c4c9bf04adc7b47f883ef7331e27cec
Aditya Mohan <adityak735@gmail.com>
2025-06-01 12:22:18 +0200

    Exploration: Added epsilon decay

--- mighty/mighty_utils/update_utils.py
+++ mighty/mighty_utils/update_utils.py
@@ -1,4 +1,3 @@
-# FIXME: EWRL: we might want to move this to a general update utils module
 def polyak_update(source_params, target_params, tau: float):
     """Polyak averaging for target network updates."""
     for source, target in zip(source_params, target_params):
