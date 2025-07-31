3c6210cc85751ff8355553f1b950914725086e7a
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2025-07-31 17:15:22 +0200

    configs are now running

--- mighty/mighty_utils/wrappers.py
+++ mighty/mighty_utils/wrappers.py
@@ -111,7 +111,9 @@ class MultiDiscreteActionWrapper(gym.Wrapper):
         self.action_mapper = {}
         for idx, prod_idx in zip(
             range(np.prod(self.env.action_space.nvec)),
-            itertools.product(*[np.arange(val) for val in self.env.action_space.nvec]),
+            itertools.product(
+                *[np.arange(val) for val in self.env.action_space.nvec]
+            ),
         ):
             self.action_mapper[idx] = prod_idx
 
